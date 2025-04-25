0bd7886c075808395ca1ddcfce90fa0a3827fab3
mrajpal2 <mrajpal2@illinois.edu>
2025-04-24 20:32:09 -0400

    Updated model.

--- netconfvalidator/utils/openconfig.py
+++ netconfvalidator/utils/openconfig.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08611a921f9e8b109048bdd4d0eb8cb271d4bbd5f9eae2fae5294382d045f17a
-size 111915225
+oid sha256:e8a4f2d235a870ad1a36f9b5bef5469fb831bb448beca565c8c55d6af9c05512
+size 114142661
